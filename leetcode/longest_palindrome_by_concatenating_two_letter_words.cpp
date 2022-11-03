637970c059ec807d19cc2b60a749eae53ca200b5
kimhyuk <kimhyuk722@live.co.kr>
2022-11-04 00:19:01 +0900

    update

---
+++ leetcode/longest_palindrome_by_concatenating_two_letter_words.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        list<string> l;
+
+        for(auto word:words){
+            l.push_back(word);
+        }
+        
+
+
+
+    }
+};
