74e6770e5b9d7dc6cb90d2b3ba7c72dc6df59184
kimhyuk <kimhyuk722@live.co.kr>
2022-12-02 16:59:42 +0900

    Add determin_if_string_halves_are_alike

---
+++ leetcode/determine_if_string_halves_are_alike.rs
@@ -0,0 +1,38 @@
+
+static VOWELS: [char; 5] = ['a', 'e', 'i', 'o', 'u'];
+
+impl Solution {
+    pub fn count_vowels(s: &str) -> i32 {
+        let mut count = 0;
+        for (i,item) in s.chars().enumerate() {
+            if VOWELS.contains(&item.to_lowercase().next().unwrap()) {
+                count += 1;
+            }
+        }
+        count
+    }
+
+    pub fn halves_are_alike(s: String) -> bool {
+        let len = s.len();
+        return Solution::count_vowels(&s[0..(len/2)]) == Solution::count_vowels(&s[(len/2)..]);
+    }
+}
+
+#[test]
+fn case1() {
+    let solution = Solution::halves_are_alike(String::from("book"));
+    assert_eq!(true,solution);
+}
+
+#[test]
+fn case2() {
+    let solution = Solution::halves_are_alike(String::from("textbook"));
+
+    assert_eq!(false,solution);
+}
+
+
+fn main(){
+
+}
+
